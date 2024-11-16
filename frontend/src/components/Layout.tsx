cdfd7a1de09c19a9807dab1523c002921d50afa9
mfaerber1198 <melifaerber@gmail.com>
2024-11-16 15:55:59 +0100

    Created QR Scanner
    Created QR Scanner

--- frontend/src/components/Layout.tsx
+++ frontend/src/components/Layout.tsx
@@ -1,17 +1,38 @@
-import * as React from 'react';
+import React, {useState} from 'react';
+import QrReader from "./QrReader";
 
 interface Props {
     children: React.ReactNode
 }
 
 export const Layout: React.FC<Props> = ({children}) => {
+    const [showQrReader, setShowQrReader] = useState(false);
+
     return (
         <div>
-            <div className='bg-white p-2.5 flex items-center shadow-md'>
-                <img src='/equipli-logo.svg' className='w-16 h-auto' alt={'equipli logo'}/>
-                <header className='ml-2 text-2xl font-semibold text-customBlue'>equipli</header>
+            <div className="bg-white p-2.5 flex items-center shadow-md">
+                <img src="/equipli-logo.svg" className="w-16 h-auto" alt="equipli logo"/>
+                <header className="ml-2 text-2xl font-semibold text-customBlue flex-grow">equipli</header>
+                <button
+                    className="bg-customBlue text-customBeige px-4 py-2 rounded hover:bg-customRed"
+                    onClick={() => setShowQrReader(true)}>
+                    Inventar ausleihen
+                </button>
             </div>
             {children}
+            {showQrReader && (
+                <div className="fixed inset-0 bg-customBlack bg-opacity-50 flex justify-center items-center z-10">
+                    <div className="bg-customBeige p-4 rounded-lg shadow-lg relative">
+                        <button
+                            className="absolute top-2 right-3.5 text-customRed text-5xl hover:text-customBlack z-20"
+                            onClick={() => setShowQrReader(false)}
+                        >
+                            &times;
+                        </button>
+                        <QrReader/>
+                    </div>
+                </div>
+            )}
         </div>
     );
 };
